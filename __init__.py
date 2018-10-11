ff2aa362cbb766d28a2637f0b56b86b7674715c0
JayEagle <41083716+JayEagle@users.noreply.github.com>
2018-10-11 11:50:13 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,11 +15,11 @@ LOG = getLogger(__name__)
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
 
-class MuseumSkill(MycroftSkill):
+class FRBKCSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(MuseumSkill, self).__init__(name="MuseumSkill")
+        super(FRBKCSkill, self).__init__(name="FRBKCSkill")
         
         # Initialize working variables used within the skill.
         self.count = 0
@@ -28,17 +28,34 @@ class MuseumSkill(MycroftSkill):
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
     # pieces, and is triggered when the user's utterance matches the pattern
     # defined by the keywords.  
-    @intent_handler(IntentBuilder("").require("museum").require("money"))
-    def handle_museum_vocab(self, message):
+    @intent_handler(IntentBuilder("").require("about").require("kc"))
+    def handle_about_kc_fed_vocab(self, message):
         # In this case, respond by simply speaking a canned response.
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("about.the.museum")
+        self.speak_dialog("about.kc.fed")
 
-    @intent_file_handler("about.museum.intent")
-    def handle_museum_intent(self, message):
-        self.speak_dialog("about.the.museum")
-        
+	@intent_handler(IntentBuilder("").require("kc").require("leader"))
+    def handle_kc_president_vocab(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("kc.president")
+
+	@intent_handler(IntentBuilder("").require("kc").require("branch"))
+    def handle_kc_branches_vocab(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("kc.branches")
+
+	@intent_handler(IntentBuilder("").require("kc").require("location"))
+    def handle_kc_district_vocab(self, message):
+        # In this case, respond by simply speaking a canned response.
+        # Mycroft will randomly speak one of the lines from the file
+        #    dialogs/en-us/hello.world.dialog
+        self.speak_dialog("kc.district")
+		
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
     # is extremely simple, there is no need to override it.  If you DO
@@ -51,5 +68,5 @@ class MuseumSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return MuseumSkill()
+    return FRBKCSkill()
 
